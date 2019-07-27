298e5785b82f8976a7929587333771be22553b1b
John Kosmicke <kozjob@yahoo.com>
2019-07-26 23:22:54 -0700

    function as prop

--- src/App.js
+++ src/App.js
@@ -12,18 +12,24 @@ class App extends Component {
   }
 
 
-  render(){
+  addNinja = (ninja) => {
+    ninja.id = Math.random();
+    let ninjas = [...this.state.ninjas, ninja];
+    this.setState({
+      ninjas: ninjas
+    });
+  }
 
+  render(){
     return (
       <div className="App">
         <h1>My first React app</h1>
         <p>Welcome!</p>
         <Ninjas ninjas={this.state.ninjas}/>
-        <AddNinja />
+        <AddNinja addNinja={this.addNinja}/>
       </div>
     );
   }
-
 }
 
 export default App;
